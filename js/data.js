34e0a1270325196921e364c2e61175b3d0e1dd51
Marek Bielańczuk <contact@mbielanczuk.com>
2011-09-11 16:08:37 +0200

    Source upload

---
+++ js/data.js
@@ -0,0 +1,144 @@
+
+
+
+[{ "name": "Jan Kowalski",
+   "desc": "Programista",
+   "values": [
+       {"from": "/Date(1309471200000)/", "to": "/Date(1310421600000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 1<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1310594400000)/", "to": "/Date(1311112800000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 2<br/><b>Description</b>: Task desc.", "customClass": "ganttOrange"}
+     ]
+},
+{ "name": "Jan Nowak",
+   "desc": "Technik",
+   "values": [
+       {"from": "/Date(1309903200000)/", "to": "/Date(1310508000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 3<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"},
+       {"from": "/Date(1312495200000)/", "to": "/Date(1313100000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 4<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Zbigniew Kowalski",
+   "desc": "Administrator",
+   "values": [
+       {"from": "/Date(1310508000000)/", "to": "/Date(1311026400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 5<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Roman Nowak",
+   "desc": "Konsultant",
+   "values": [
+       {"from": "/Date(1309471200000)/", "to": "/Date(1310162400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 6<br/><b>Description</b>: Task desc.", "customClass": "ganttGreen"},
+       {"from": "/Date(1312236000000)/", "to": "/Date(1312495200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 7<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Stanisław Kowalski",
+   "desc": "Programista",
+   "values": [
+       {"from": "/Date(1309903200000)/", "to": "/Date(1310076000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 8<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1313618400000)/", "to": "/Date(1313791200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 9<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"},
+       {"from": "/Date(1313013600000)/", "to": "/Date(1313186400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 10<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Zygmunt Nowak",
+   "desc": "Serwisant",
+   "values": [
+       {"from": "/Date(1312149600000)/", "to": "/Date(1312495200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 11<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1312754400000)/", "to": "/Date(1313359200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 12<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"},
+       {"from": "/Date(1309903200000)/", "to": "/Date(1310335200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 13<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Krzysztof Kowalski",
+   "desc": "Konsultant",
+   "values": [
+       {"from": "/Date(1311285600000)/", "to": "/Date(1311631200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 14<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1311026400000)/", "to": "/Date(1311112800000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 15<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Przemysław Nowak",
+   "desc": "Technik",
+   "values": [
+       {"from": "/Date(1313445600000)/", "to": "/Date(1314309600000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 16<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1313532000000)/", "to": "/Date(1314050400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 17<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Emil Kowalski",
+   "desc": "Administrator",
+   "values": [
+       {"from": "/Date(1310767200000)/", "to": "/Date(1311631200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 18<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"},
+       {"from": "/Date(1310853600000)/", "to": "/Date(1311372000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 19<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Dominik Nowak",
+   "desc": "Konsultant",
+   "values": [
+       {"from": "/Date(1311804000000)/", "to": "/Date(1312322400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 20<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1312581600000)/", "to": "/Date(1313272800000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 21<br/><b>Description</b>: Task desc.", "customClass": "ganttOrange"}
+     ]
+},
+{ "name": "Łukasz Kowalski",
+   "desc": "Programista",
+   "values": [
+       {"from": "/Date(1311458400000)/", "to": "/Date(1311285600000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 22<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Maciej Nowak",
+   "desc": "Serwisant",
+   "values": [
+       {"from": "/Date(1310767200000)/", "to": "/Date(1310940000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 23<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1313359200000)/", "to": "/Date(1313704800000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 24<br/><b>Description</b>: Task desc.", "customClass": "ganttOrange"}
+     ]
+},
+{ "name": "Paweł Kowalski",
+   "desc": "Programista",
+   "values": [
+       {"from": "/Date(1310508000000)/", "to": "/Date(1311026400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 25<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Piotr Nowak",
+   "desc": "Konsultant",
+   "values": [
+       {"from": "/Date(1310248800000)/", "to": "/Date(1311026400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 26<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"},
+       {"from": "/Date(1313100000000)/", "to": "/Date(1313359200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 27<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Marcin Kowalski",
+   "desc": "Programista",
+   "values": [
+       {"from": "/Date(1310767200000)/", "to": "/Date(1310940000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 28<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1313532000000)/", "to": "/Date(1313964000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 29<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"},
+       {"from": "/Date(1313272800000)/", "to": "/Date(1313445600000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 30<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Jan Nowak",
+   "desc": "Serwisant",
+   "values": [
+       {"from": "/Date(1313013600000)/", "to": "/Date(1313186400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 31<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1313618400000)/", "to": "/Date(1314223200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 32<br/><b>Description</b>: Task desc.", "customClass": "ganttOrange"},
+       {"from": "/Date(1310767200000)/", "to": "/Date(1311199200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 33<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Jan Kowalski",
+   "desc": "Programista",
+   "values": [
+       {"from": "/Date(1311112800000)/", "to": "/Date(1311544800000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 34<br/><b>Description</b>: Task desc."},
+       {"from": "/Date(1310767200000)/", "to": "/Date(1311717600000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 35<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"}
+     ]
+},
+{ "name": "Jan Nowak",
+   "desc": "Technik",
+   "values": [
+       {"from": "/Date(1314309600000)/", "to": "/Date(1314396000000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 36<br/><b>Description</b>: Task desc.", "customClass": "ganttOrange"},
+       {"from": "/Date(1314050400000)/", "to": "/Date(1314482400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 37<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Jan Kowalski",
+   "desc": "Konsultant",
+   "values": [
+       {"from": "/Date(1310594400000)/", "to": "/Date(1311458400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 38<br/><b>Description</b>: Task desc.", "customClass": "ganttOrange"},
+       {"from": "/Date(1310680800000)/", "to": "/Date(1311199200000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 39<br/><b>Description</b>: Task desc."}
+     ]
+},
+{ "name": "Jan Nowak",
+   "desc": "Serwisant",
+   "values": [
+       {"from": "/Date(1311544800000)/", "to": "/Date(1312581600000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 40<br/><b>Description</b>: Task desc.", "customClass": "ganttRed"},
+       {"from": "/Date(1312322400000)/", "to": "/Date(1312754400000)/", "desc": "<b>Type</b>: Task<br/><b>name</b>: Task 41<br/><b>Description</b>: Task desc."}
+     ]
+}]
